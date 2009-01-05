019a97fcd42eca8428690b2ad59f1f5d98cf99f3
pluskid <pluskid@gmail.com>
2009-01-05 15:12:47 +0800

    Initial commit

---
+++ pyml/clustering/kmedoids.py
@@ -0,0 +1,62 @@
+import random
+import numpy.matlib as ml
+from numpy import array, ndarray, zeros, inf
+
+def kmedoids(distmat, k, threshold=1e-15, maxiter=300,
+        seeds=None):
+    """\
+    k-medoids clustering algorithm.
+      distmat: a N-by-N dissimilarity matrix.
+      k: number of clusters
+      threshold: the threshold to stop the iteration
+      maxiter: max iteration steps
+      seeds: a length-k int array of index (into distmat)
+             representing the initial selection of medoid 
+             points.
+
+    return (imedoids, labels)
+      imedoids: a length-k int array of index indicating
+                the points selected as final medoids.
+      labels: a length-N int array of the label for each
+              point.
+    """
+    N = len(distmat)
+    # initialize medoids
+    if seeds is None:
+        imedoids = random.sample(range(N), k)
+    else:
+        if not isinstance(seeds, ndarray):
+            seeds = array(seeds)
+        imedoids = seeds
+        if imedoids.shape != (k,):
+            raise ValueError('seeds should be a length-%d array' % k)
+
+    # initialize J
+    Jprev = inf
+    # initialize iteration count
+    iter = 0
+
+    # iterations
+    while True:
+        # distance from medoids to all other points
+        dist = distmat[imedoids]
+        # assign x to nearst medoid
+        labels = dist.argmin(axis=0)
+        J = 0
+        # re-choose each medoids
+        for j in range(k):
+            idx_j = (labels == j).nonzero()[0]
+            distj = distmat[idx_j][:, idx_j]
+            distsum = ml.sum(distj, axis=1)
+            idxmin = distsum.argmin()
+            imedoids[j] = idx_j[idxmin]
+            J += distsum[idxmin]
+
+        if Jprev-J < threshold:
+            break
+        Jprev = J
+        iter += 1
+        if iter >= maxiter:
+            break
+
+    return imedoids, labels
